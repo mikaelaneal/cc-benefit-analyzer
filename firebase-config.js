0581e1be01550e578b335fecf3eddba5b060f642
mikaelaneal <111027903+mikaelaneal@users.noreply.github.com>
2025-02-06 22:01:26 -0600

    Update firebase-config.js correcting typo

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyAVYg81E9eoyllIOR1__N48fWzEOqor-L8",
     authDomain: "credit-cards-rewards-tracker.firebaseapp.com",
     projectId: "credit-cards-rewards-tracker",
-    storageBucket: "credit-cards-rewards-tracker.fappspot.com",
+    storageBucket: "credit-cards-rewards-tracker.appspot.com",
     messagingSenderId: "282954536684",
     appId: "1:282954536684:web:72ca614a012f554cd437c9",
     measurementId: "G-C5ZCGZ4L5P"
